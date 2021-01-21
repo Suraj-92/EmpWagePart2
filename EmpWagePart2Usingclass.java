12901edd4f369b5e689ca10dd5a044d4cecba699
Suraj-92 <suraj.temkar.92@gmail.com>
2021-01-21 19:47:37 +0530

    First UC done

--- EmpWagePart2Usingclass.java
+++ EmpWagePart2Usingclass.java
@@ -1,5 +1,11 @@
 public class EmpWagePart2Usingclass{
 	public static void main(String[] args){
-	System.out.println("Welcome Employee");
-}
+		int isFullTime=1;
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		
+		if( empCheck == isFullTime )
+			System.out.println("Employee is present");
+		else		
+			System.out.println("Employee is absent");
+	}
 }
