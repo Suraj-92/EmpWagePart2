a03443e1962e79450fa83c329c255121fcbad8bd
Suraj-92 <suraj.temkar.92@gmail.com>
2021-01-24 19:07:53 +0530

    Use Instance Variable Use Case-9

--- EmpWagePart2Usingclass.java
+++ EmpWagePart2Usingclass.java
@@ -16,37 +16,43 @@ public class EmpWagePart2Usingclass{
 		this.maxHoursPerMonth=maxHoursPerMonth;
 	}
 	
+private int computeWage(){
+	 int empHrs=0;
+   int totalEmpHrs=0;
+   int totalWorkingDays=0;
+
+   while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+   {
+      totalWorkingDays++;
+      int empCheck=(int) Math.floor(Math.random() * 10) %3;
+
+      switch (empCheck){
+         case isPartTime:
+                     empHrs=4;
+                  break;
+         case isFullTime:
+                     empHrs=8;
+                  break;
+         default:
+               empHrs=0;
+   }
+         totalEmpHrs +=empHrs;
+         System.out.println("Days: " + totalWorkingDays + "Emp Hrs: " +empHrs);
+
+   }
+return totalEmpHrs * empRatePerHour;
+
+}
+
 public static void main(String[] args) 
 {
 	EmpWagePart2Usingclass emp=new EmpWagePart2Usingclass("DMart",20,2,10);
-
-	
-	int empHrs=0;
-	int totalEmpHrs=0;
-	int totalWorkingDays=0;
-
-	while(totalEmpHrs <= emp.maxHoursPerMonth && totalWorkingDays < emp.numOfWorkingDays)
-	{
-		totalWorkingDays++;
-		int empCheck=(int) Math.floor(Math.random() * 10) %3;
-				
-		switch (empCheck){
-			case isPartTime:
-							empHrs=4;
-						break;
-			case isFullTime:
-							empHrs=8;
-						break;
-			default:
-					empHrs=0;
-	}
-			totalEmpHrs +=empHrs;
-			System.out.println("Days: " + totalWorkingDays + "Emp Hrs: " +empHrs);
-
-	}
-int totalEmpWage=totalEmpHrs * emp.empRatePerHour;
-System.out.println("Total Emp Wage for Company: " + emp.company + " is : " +totalEmpWage);
-
-}
+	EmpWagePart2Usingclass emp1=new EmpWagePart2Usingclass("Reliance",30,3,10);
+	EmpWagePart2Usingclass emp2=new EmpWagePart2Usingclass("Info",30,3,10);
+	System.out.println("Total Emp Wage For Company " +emp.company + " is: " + emp.computeWage());
+   System.out.println("Total Emp Wage For Company " +emp1.company + " is: " + emp1.computeWage());
+	System.out.println("Total Emp Wage For Company " +emp2.company + " is: " + emp2.computeWage());
 }
  
+}
+
